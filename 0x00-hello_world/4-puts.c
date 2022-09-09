96540b97b15e838682caa8f42e574d1953b608f2
kismoflan <amoskipkemei12#gmail.com>
2022-09-08 17:19:42 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,17 @@
 
 /**
  *
- *  * main - Entry point
+ *  
+ * main - Entry point
  *
- *   *
+ *   
  *
- *    * Return: Always 0 (Success)
  *
- *     */
+ *    
+ * Return: Always 0 (Success)
+ *
+ *     
+ */
 
 int main(void)
 	
