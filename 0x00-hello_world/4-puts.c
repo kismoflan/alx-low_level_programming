126dfa909db175c88ec78239d9c4e12047049098
kismoflan <amoskipkemei12#gmail.com>
2022-09-08 17:33:34 -0700

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#include <stdio.h> /** * main - Entry point * * Return: Always 0 (Success) */int main(void){	puts("\"Programming is like building a multilingual puzzle");	return (0);}
+#include <stdio.h>
+/**
+ *
+ * main - entry point
+ * 
+ * Return: 0
+ * 
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
