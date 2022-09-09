0be76761bee2c48bd09feafb6f544863b39916fc
kismoflan <amoskipkemei12#gmail.com>
2022-09-08 17:16:43 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,23 @@
-#include <unistd.h>
+#include <stdio.h>
+
+
+
 /**
- *  
- * main - prints the standard error sandwiched around the  standard output
- * Return:returns 1
- *  */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+	
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
-}
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}}
