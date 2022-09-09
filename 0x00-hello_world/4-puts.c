24f6c11004ab0fae768aef53e57d09b4d07e0f86
kismoflan <amoskipkemei12#gmail.com>
2022-09-08 17:35:14 -0700

    no dey work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
  *
- * main - entry point
- * 
- * Return: 0
- * 
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
