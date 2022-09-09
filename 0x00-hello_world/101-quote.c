42f18b295231f721e5b5cc8588fa1df9bc5f2081
kismoflan <amoskipkemei12#gmail.com>
2022-09-08 18:17:46 -0700

    pt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,35 +1,12 @@
-#include <stdio.h>
-
-#include <string.h>
-
 #include <unistd.h>
-
 /**
+ * main - prints the standard error sandwiched around the  standard output
  *
- *  * main - main block
+ * Return:returns 1
  *
- *   * Return: 1
- *
- *    */
-
+ */
 int main(void)
-
 {
-
-		int s;
-
-
-
-			s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-
-
-				write(2,
-
-							      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-
-							      	      s);
-
-					return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
