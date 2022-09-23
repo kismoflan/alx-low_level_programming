e6c1fed2e083ff0bea178134d40d042d8c4818ff
kismoflan <62714337+kismoflan@users.noreply.github.com>
2022-09-23 17:26:35 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,18 @@
 #include "main.h"
+#include "holberton.h"
+
 /**
  * _strcmp - compares two strings
- * @s1: input parameter
- * @s2: input parameter
+ * @s1: first string
+ * @s2: second string
  *
- * Return: 0 if equal or b if is different
+ * Return: outputs the difference of s1 and s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int R;
+	int i = 0;
 
-	i = 0;
-	
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
-	{
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
 		i++;
-	}
-	R = s1[i] - s2[i];
-	return (R);
+	return (*(s1 + i) - *(s2 + i));
 }
