3168df66e28b71bb436d92954e588aa96a2d470c
kismoflan <62714337+kismoflan@users.noreply.github.com>
2022-09-23 17:41:05 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int a = 0;
+	int b = 0;
 
-	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
-		i++;
-	return (*(s1 + i) - *(s2 + i));
+	while (s1[a] != '\0' && b == 0)
+	{
+		b = s1[a] - s2[a];
+		a++;
+	}
+	return (b);
 }
