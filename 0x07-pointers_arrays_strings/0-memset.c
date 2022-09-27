d15e73add2be5ac55ba70fe0e0fec135dd91c046
kismoflan <62714337+kismoflan@users.noreply.github.com>
2022-09-27 15:21:50 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,46 +1,20 @@
-nclude "holberton.h"
-
+#include "main.h"
 /**
+ * _memset - fills memory with constant byte
+ * @s: pointer to the memory area
+ * @n: bytes of memory
+ * @b: constant byte
  *
- *  * _memset - fills memory with constant byte
- *
- *   * @s: pointer to the memory area
- *
- *    * @n: bytes of memory
- *
- *     * @b: constant byte
- *
- *      *
- *
- *       * Return: the memory area of s
- *
- *        */
-
+ * Return: the memory area of s
+ */
 char *_memset(char *s, char b, unsigned int n)
-
 {
+	unsigned int i = 0;
 
-		unsigned int i = 0;
-
-
-
-			while (i < n)
-
-					{
-
-								s[i] = b;
-
-										i++;
-
-											}
-
-				return (s);
-
-
-
-
-
-
-
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
-
