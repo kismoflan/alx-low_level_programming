75c2d27c81b9e646d124dd95b58ac5d82cd24f27
kismoflan <amoskipkemei12@gmail.com>
2024-03-18 12:19:33 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet gam

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_alphabet - check the code for ALX School students.
- *
- * Return: Always 0.
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
  */
-
 void print_alphabet(void)
 {
-	char alp = 'a';
+	char i;
 
-	while (alp <= 'z')
-	{
-		_putchar (alp);
-		alp++;
-
-	}
-	_putchar ('\n');
-
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
